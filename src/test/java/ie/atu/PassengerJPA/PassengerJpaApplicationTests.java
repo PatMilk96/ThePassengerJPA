bea07ddfa93dbe803e4d38d388ef6c90cf4582ef
Patryk Milkiewicz <g00397265@atu.ie>
2022-12-07 22:28:46 +0000

    Started new Passenger JPA project

---
+++ src/test/java/ie/atu/PassengerJPA/PassengerJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.PassengerJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassengerJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
